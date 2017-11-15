3989b022ad707661953ad94648ac02f65baa9b82
Keano <keanov@gmail.com>
2017-11-14 21:09:45 -0800

    init project

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+
+        <title></title>
+
+    </head>
+    <body>
+        <h1>About Us</h1>
+    </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+
+        <title></title>
+
+    </head>
+    <body>
+        <h1>Hello, World</h1>
+    </body>
+</html>
